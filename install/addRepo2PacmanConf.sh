42f02c844fbe0c184cb23749b94ec8e562ef6819
key <46032999+keyaedisa@users.noreply.github.com>
2022-11-14 04:12:39 -0500

    Update addRepo2PacmanConf.sh

--- install/addRepo2PacmanConf.sh
+++ install/addRepo2PacmanConf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Automatically adding Valen Repo to your /etc/pacman.conf"
+curl -fsSL https://raw.githubusercontent.com/keyaedisa/valen_repo/master/README.md
 sed -n '11,14p' README.md >> /etc/pacman.conf
+rm README.md
 echo "Updating pacman databases"
 pacman -Sy
