4441c5e4611a3389352c93842f06182d1e337d9c
keyaedisa <46032999+keyaedisa@users.noreply.github.com>
2022-11-14 04:45:31 -0500

    trying setup keys

--- install/addRepo2PacmanConf.sh
+++ install/addRepo2PacmanConf.sh
@@ -5,4 +5,6 @@ wget https://raw.githubusercontent.com/keyaedisa/valen_repo/master/README.md
 sed -n '11,14p' README.md >> /etc/pacman.conf
 rm README.md
 echo "Updating pacman databases"
+gpg --keyserver keyserver.ubuntu.com --recv-keys A2D9ED6622D9E188C240702DE848A1C3669D2EBE
+pacman-key -r keyserver.ubuntu.com A2D9ED6622D9E188C240702DE848A1C3669D2EBE
 pacman -Sy
