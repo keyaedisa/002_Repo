452221710d8be6e4746401f109a3ea4a7198bcba
keyaedisa <46032999+keyaedisa@users.noreply.github.com>
2023-03-04 01:59:44 -0500

    reapri

--- install/addRepo2PacmanConf.sh
+++ install/addRepo2PacmanConf.sh
@@ -8,7 +8,7 @@ if [[ "$1" == "dev" ]]; then
 	sed -n '19,22p' README.md >> /etc/pacman.conf
 	rm README.md
 	echo "Updating pacman databases"
-	wget https://raw.githubusercontent.com/keyaedisa/valen_repo/master/install/repo-Arch-public-key.gpg
+	curl https://raw.githubusercontent.com/keyaedisa/valen_repo/master/install/repo-Arch-public-key.gpg
 	pacman-key -a repo-Arch-public-key.gpg
 	pacman-key --finger w0rldEater
 	pacman-key --lsign-key w0rldEater
